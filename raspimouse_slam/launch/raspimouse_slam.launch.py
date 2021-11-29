c297089609d413ab9d94be4391be277503765bd2
Shuhei Kozasa <kozasa@rt-net.jp>
2021-11-29 15:50:13 +0900

    Adds launch files

---
+++ raspimouse_slam/launch/raspimouse_slam.launch.py
@@ -0,0 +1,49 @@
+# Copyright 2021 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDesription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    slam_noede = Node(
+        package='slam_toolbox', executable='sync_slam_toolbox_node',
+        output='screen',
+        parameters=[ ],
+    )
+
+    rviz2_node = Node(
+        name='rviz2',
+        package='rviz2', executable='rviz2', output='screen',
+        arguments=[
+            '-d',
+            get_package_share_directory('raspimouse_slam')
+            + '/rviz/default.rviz'],
+    )
+
+    static_transform_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher', output='screen',
+        arguments=['0', '0', '0.1', '0', '3.14',
+                   '3.14', 'base_footprint', 'laser'],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(slam_node)
+    ld.add_action(rviz2_node)
+    ld.add_action(static_transform_publisher_node)
+
+    return ld
---
+++ raspimouse_slam/launch/teleop.launch.py
@@ -0,0 +1,93 @@
+# Copyright 2021 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch import LaunchIntrospector
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.actions import OpaqueFunction
+from launch.actions import SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
+
+
+def generate_launch_description():
+    joydev = LaunchConfiguration('joydev')
+    declare_joydev = DeclareLaunchArgument(
+        'joydev', default_value='/dev/input/js0',
+        description='Device file for JoyStick Controller'
+    )
+
+    declare_joyconfig = DeclareLaunchArgument(
+        'joyconfig', default_value='f710',
+        description='Keyconfig of joystick controllers: supported: f710, dualshock3'
+    )
+
+    declare_mouse = DeclareLaunchArgument(
+        'mouse', default_value="true",
+        description='Launch raspimouse node'
+    )
+
+    def func_get_joyconfig_file_name(context):
+        param_file = os.path.join(
+            get_package_share_directory('raspimouse_ros2_examples'),
+            'config',
+            'joy_' + context.launch_configurations['joyconfig'] + '.yml')
+
+        if os.path.exists(param_file):
+            return [SetLaunchConfiguration('joyconfig_filename', param_file)]
+        else:
+            return [LogInfo(msg=param_file + " is not exist.")]
+    get_joyconfig_file_name = OpaqueFunction(function=func_get_joyconfig_file_name)
+
+    joy_node = Node(
+        package='joy_linux',
+        executable='joy_linux_node',
+        parameters=[{'dev': joydev}]
+    )
+
+    joystick_control_node = Node(
+        package='raspimouse_ros2_examples',
+        executable='joystick_control.py',
+        parameters=[LaunchConfiguration('joyconfig_filename')]
+    )
+
+    def func_launch_mouse_node(context):
+        if context.launch_configurations['mouse'] == "true":
+            return [LifecycleNode(
+                name='raspimouse',
+                package='raspimouse', executable='raspimouse', output='screen',
+                parameters=[os.path.join(get_package_share_directory(
+                    'raspimouse_ros2_examples'), 'config', 'mouse.yml')]
+            )]
+    mouse_node = OpaqueFunction(function=func_launch_mouse_node)
+
+    ld = LaunchDescription()
+    ld.add_action(declare_joydev)
+    ld.add_action(declare_joyconfig)
+    ld.add_action(declare_mouse)
+
+    ld.add_action(get_joyconfig_file_name)
+
+    ld.add_action(joy_node)
+    ld.add_action(joystick_control_node)
+    ld.add_action(mouse_node)
+
+    print(LaunchIntrospector().format_launch_description(ld))
+
+    return ld
