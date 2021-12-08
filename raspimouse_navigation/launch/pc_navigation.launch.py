008933cc4eaa6550e9a6b5c3ca1d97dc911751a3
Shuhei Kozasa <kozasa@rt-net.jp>
2021-12-08 15:47:59 +0900

    Update launch file. Still wip but a confirmed launch

--- raspimouse_navigation/launch/pc_navigation.launch.py
+++ raspimouse_navigation/launch/pc_navigation.launch.py
@@ -16,46 +16,44 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    map_file = LaunchConfiguration('map_file')
+    param_file = LaunchConfiguration('param_file')
+    rviz2_file = LaunchConfiguration('rviz2_file')
 
     declare_robot = DeclareLaunchArgument(
         'robot', default_value='raspimouse',
         description='The name of the robot.'
     )
 
-    # map_dir = DeclareLaunchArgument(
-    #    'map_file', default_value=os.path.join(
-    map_dir = LaunchConfiguration(
-        'map_file', default=os.path.join(
+    map_dir = DeclareLaunchArgument(
+        'map_file', default_value=os.path.join(
             get_package_share_directory('raspimouse_slam'),
             'maps',
             'test.yaml'),
-    #    description='The path to the map file.'
+        description='The path to the map file.'
     )
     
-    # param_dir = DeclareLaunchArgument
-    #    'param_file', default_value=os.path.join(
-    param_dir = LaunchConfiguration(
-        'param_file', default=os.path.join(
+    param_dir = DeclareLaunchArgument(
+        'param_file', default_value=os.path.join(
             get_package_share_directory('raspimouse_navigation'),
             'param',
             'raspimouse.yaml'),
-    #    description='The path to the param file.'
+        description='The path to the param file.'
     )
 
-    rviz2_config_dir = LaunchConfiguration(
-        'rviz_file', default=os.path.join(
+    rviz2_config_dir = DeclareLaunchArgument(
+        'rviz2_file', default_value=os.path.join(
             get_package_share_directory('nav2_bringup'),
             'rviz',
-            'nav2_default_view.rviz')
+            'nav2_default_view.rviz'),
+        description='Path to rviz file'
     )
 
     nav2_launch_file_dir = os.path.join(
@@ -66,25 +64,23 @@ def generate_launch_description():
     nav2_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
         launch_arguments={
-            'map': 'map_dir',
-            'params_file': LaunchConfiguration[param_dir],
+            'map': map_file,
+            'params_file': param_file,
             'use_sim_time': use_sim_time}.items(),
     )
 
     rviz2_node = Node(
         name='rviz2',
         package='rviz2', executable='rviz2', output='screen',
-        arguments=[
-            '-d', rviz2_config_dir],
-        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=['-d', rviz2_file],
+        parameters=[{'use_sim_time': use_sim_time}]
     )
-
 
     ld = LaunchDescription()
     ld.add_action(declare_robot)
     ld.add_action(map_dir)
     ld.add_action(param_dir)
-    #ld.add_action(rviz2_config_dir)
+    ld.add_action(rviz2_config_dir)
 
     ld.add_action(nav2_node)
     ld.add_action(rviz2_node)
