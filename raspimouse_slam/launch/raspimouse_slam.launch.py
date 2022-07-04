f62b665d08107d1c61a62ee3427de59e1b383968
Shuhei Kozasa <kozasa@rt-net.jp>
2022-07-04 17:24:04 +0900

    Removes static tf node. raspimouse_description has that covered

--- raspimouse_slam/launch/raspimouse_slam.launch.py
+++ raspimouse_slam/launch/raspimouse_slam.launch.py
@@ -16,21 +16,9 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, LaunchConfigurationEquals
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    declare_use_lds = DeclareLaunchArgument(
-        'use_lds',
-        default_value='false',
-        description='Set "true" when using lds.')
-
-    declare_use_urg = DeclareLaunchArgument(
-        'use_urg',
-        default_value='false',
-        description='Set "true" when using urg.')
-
     declare_arg_lidar_frame = DeclareLaunchArgument(
         'lidar_frame',
         default_value='laser',
@@ -60,33 +48,11 @@ def generate_launch_description():
             + '/rviz/default.rviz'],
     )
 
-    static_tf_lds_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher', output='screen',
-        arguments=['0', '0', '0.01', '0', '3.14', '3.14',
-                    'base_footprint', LaunchConfiguration('lidar_frame')],
-        # condition=IfCondition(LaunchConfiguration('use_lds'))
-        condition=LaunchConfigurationEquals('lidar', 'lds')
-    )
-
-    static_tf_urg_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher', output='screen',
-        arguments=['0', '0', '0.01', '0', '0', '0',
-                    'base_footprint', LaunchConfiguration('lidar_frame')],
-        # condition=IfCondition(LaunchConfiguration('use_urg'))
-        condition=LaunchConfigurationEquals('lidar', 'urg')
-    )
-
     ld = LaunchDescription()
-    ld.add_action(declare_use_lds)
-    ld.add_action(declare_use_urg)
     ld.add_action(declare_arg_lidar_frame)
     ld.add_action(declare_arg_lidar)
 
     ld.add_action(slam_node)
     ld.add_action(rviz2_node)
-    ld.add_action(static_tf_lds_node)
-    ld.add_action(static_tf_urg_node)
 
     return ld
