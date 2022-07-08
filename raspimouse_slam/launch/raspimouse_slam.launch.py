0df88327aabd2ce4314fc214ac615121addaa306
Shuhei Kozasa <kozasa@rt-net.jp>
2022-07-08 17:26:40 +0900

    adds library to import

--- raspimouse_slam/launch/raspimouse_slam.launch.py
+++ raspimouse_slam/launch/raspimouse_slam.launch.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
