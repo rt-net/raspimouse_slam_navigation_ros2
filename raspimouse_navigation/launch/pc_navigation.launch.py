be4e59fbc2f21bb40a1ae51862f89a2bf3c3bd67
Shuhei Kozasa <kozasa@rt-net.jp>
2022-07-12 17:04:04 +0900

    Update license year

--- raspimouse_navigation/launch/pc_navigation.launch.py
+++ raspimouse_navigation/launch/pc_navigation.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 RT Corporation
+# Copyright 2022 RT Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
