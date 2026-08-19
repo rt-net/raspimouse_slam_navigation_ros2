1d8258177fdfefba721646fb1cac3195a895756a
KuraZuzu <kurasawa@rt-net.jp>
2026-08-19 11:13:14 +0900

    setup.pyの更新

--- raspimouse_navigation_examples/setup.py
+++ raspimouse_navigation_examples/setup.py
@@ -15,12 +15,12 @@ setup(
         ('share/' + package_name + '/launch', ['launch/example.launch.py']),
     ],
     install_requires=['setuptools'],
+    extras_require={'test': ['pytest']},
     zip_safe=True,
     maintainer='RT Corporation',
     maintainer_email='shop@rt-net.jp',
     description='Navigation sample package for Raspberry Pi Mouse V3',
     license='Apache License 2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'waypoint = raspimouse_navigation_examples.waypoint:main',
